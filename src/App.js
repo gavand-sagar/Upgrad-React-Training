ef874115caeab3c98b4775812086e9f21840e493
gavand-sagar <gavand.sagar.s@gmail.com>
2025-11-20 12:16:47 +0530

    Routes moved to other files  (Good practice)

--- src/App.js
+++ src/App.js
@@ -2,50 +2,21 @@
 import { AmazonButton } from './AmazonButton';
 import './App.css';
 import { Button, TextField, Rating } from '@mui/material'
-import StateDemo from './StateDemo';
-import ColorChangeWithState from './ColorChangeWithState';
-import HideShow from './HideShow';
-import ToggleButton from './ToggleButton';
-import CustomAttributes from './CustomAttributes';
-import CustomEvents from './CustomEvents';
-import ListAndStates from './ListAndStates';
-import UseEffectBasics from './UseEffectBasics';
-import UseRefDemo from './UseRefDemo';
-import UseRefForForms from './UseRefForForms';
-import ControlledForm from './ControlledForm';
-import ReactHookForm from './ReactHookForm';
-import ReactHookFormMUI from './ReactHookFormMUI';
-import SimpleTodoApp from './SimpleTodoApp';
 import { Link, Route, Router, Routes } from 'react-router-dom';
+import { routes } from './Routes';
 
 function App() {
-  const routes = [
-    {
-      path: '/StateDemo',
-      linkText: "State Demo",
-      element: <StateDemo />
-    },
-    {
-      path: '/SimpleTodoApp',
-      linkText: "Simple Todo App",
-      element: <SimpleTodoApp />
-    },
-    {
-      path: '/ReactHookFormMUI',
-      linkText: "React Hook Form MUI",
-      element: <ReactHookFormMUI />
-    },
-  ]
+ 
   return (
     <div className="App">
       <nav>
         {
-          routes.map(x => <Link to={x.path}><Button sx={{ margin: '5px' }} variant='contained'>{x.linkText}</Button></Link>)
+          routes?.map(x => <Link to={x.path}><Button sx={{ margin: '5px' }} variant='contained'>{x.linkText}</Button></Link>)
         }
       </nav>
       <Routes>
         {
-          routes.map(x => <Route path={x.path} element={x.element} />)
+          routes?.map(x => <Route path={x.path} element={x.element} />)
         }
       </Routes>
     </div>
---
+++ src/Routes.js
@@ -0,0 +1,32 @@
+import StateDemo from './StateDemo';
+import ColorChangeWithState from './ColorChangeWithState';
+import HideShow from './HideShow';
+import ToggleButton from './ToggleButton';
+import CustomAttributes from './CustomAttributes';
+import CustomEvents from './CustomEvents';
+import ListAndStates from './ListAndStates';
+import UseEffectBasics from './UseEffectBasics';
+import UseRefDemo from './UseRefDemo';
+import UseRefForForms from './UseRefForForms';
+import ControlledForm from './ControlledForm';
+import ReactHookForm from './ReactHookForm';
+import ReactHookFormMUI from './ReactHookFormMUI';
+import SimpleTodoApp from './SimpleTodoApp';
+
+export const routes = [
+    {
+      path: '/StateDemo',
+      linkText: "State Demo",
+      element: <StateDemo />
+    },
+    {
+      path: '/SimpleTodoApp',
+      linkText: "Simple Todo App",
+      element: <SimpleTodoApp />
+    },
+    {
+      path: '/ReactHookFormMUI',
+      linkText: "React Hook Form MUI",
+      element: <ReactHookFormMUI />
+    },
+  ]

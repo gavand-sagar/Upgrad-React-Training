dc9211c93c6df39fe39127aa39ec57c1a2cc3513
gavand-sagar <gavand.sagar.s@gmail.com>
2025-11-24 12:32:21 +0530

    style components As HOC

--- src/HOCDemo.jsx
+++ src/HOCDemo.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import SimpleTodoApp from './SimpleTodoApp'
 import { Button, CircularProgress, Rating } from '@mui/material'
 import StateDemo from './StateDemo'
+import { styled } from '@mui/system';
+
+const MyCustomizedButton = styled(Button)({
+  color: 'white',
+  backgroundColor: 'green',
+  padding: 8,
+  borderRadius: 4,
+});
 
 //Higher Order functions - any function that takes other function as argument and returns new function
 
@@ -54,6 +62,9 @@ export default function HOCDemo() {
             {/* <RatingWithLoading onChange={(e) => alert(e.target.value)} value={4} loading={true} /> */}
 
             {/* <Button loading={true} variant='contained'>Login</Button> */}
+        
+            <Button>NORMAL MUI BUTTON</Button>
+            <MyCustomizedButton>THIS IS Customized with HOC</MyCustomizedButton>
         </div>
     )
 }
